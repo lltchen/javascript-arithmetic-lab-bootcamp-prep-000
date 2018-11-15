573cab3011fd6e57241ff88c3d88dca9c7f3d86a
lltchen <lltchen1@gmail.com>
2018-11-15 01:12:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function add(){
-  1 + 80
-}
-subtract(60, 40) // 20
-multiply(2, 3.4) // 6.8 (there's that floating-point arithmetic again...)
-divide(5.0, 2.5) // 2
-subtract(), multiply(), divide()
+add(a,b
+subtract(a,b)
+multiply(a,b)
+divide(a,b)
