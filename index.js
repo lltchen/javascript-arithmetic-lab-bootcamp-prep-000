9b258457ed4bda22d687f7be1b07a6175e0a0306
lltchen <lltchen1@gmail.com>
2018-11-15 01:08:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function add(1 , 80)
+function add(){
+  1 + 80
+}
 subtract(60, 40) // 20
 multiply(2, 3.4) // 6.8 (there's that floating-point arithmetic again...)
 divide(5.0, 2.5) // 2
+subtract(), multiply(), divide()
